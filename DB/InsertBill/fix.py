f47b9ae5a63baa34bfc41af855e98602d23a7f7c
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2021-10-25 00:34:26 +0700

    Insert bill
    - To run this script, please copy content file to script sql

---
+++ DB/InsertBill/fix.py
@@ -0,0 +1,36 @@
+from math import prod
+import random
+
+
+def readData():
+    product_id = []
+    price = []
+    f = open("E:\HTML\SanPham.txt", errors="ignore")
+    for line in f:
+        info = line.split("INSERT INTO SanPham(MaSP, TenSP, SoLuongTon, MoTa, Gia) VALUES ")
+        data = info[-1].split(",")
+        product_id.append(data[0][2:-1])
+        price.append(int(data[-1][:-2]))
+    return product_id, price
+
+
+def writeData(product_id, price):
+    i = 1
+    for j in range(6, 11):
+        f = open(f"CT_HoaDon#{j}.txt", "w")
+        while i <= (j-6+1)*100000:
+            quantity = random.randint(1, 10)
+            sale = random.randint(1, 5)
+            index = random.randint(0, 9999)
+            price_before = int(price[index] * sale * 0.1)
+            price_after = int(price[index] * sale * 0.01)
+            f.write("INSERT [dbo].[CT_HoaDon] ([MaHD], [MaSP], [SoLuong], [GiaBan], [GiaGiam], [ThanhTien]) VALUES ")
+            f.write(f"(N'HD{(6 - len(str(i))) * '0' + str(i)}', N'{product_id[index]}, {quantity}, {price_before}, {price_after}, {quantity*(price_before-price_after)})\n")
+            if i % 100 == 0:
+                f.write("GO\n")
+            i += 1
+        f.close()
+
+
+product_id, price = readData()
+writeData(product_id, price)
---
+++ DB/InsertBill/tool.py
@@ -0,0 +1,62 @@
+from random import randrange
+from datetime import timedelta, datetime
+from random import randint
+
+
+def random_date(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+
+def get_date():
+    start_date = datetime.strptime('5/1/2020 12:01 AM', '%m/%d/%Y %I:%M %p')
+    end_date = datetime.strptime('6/30/2021 11:59 PM', '%m/%d/%Y %I:%M %p')
+    return random_date(start_date, end_date)
+
+
+def writeToScript(bill_id, customer_id, total_bill):
+    i = 0
+    for i in range(5):
+        f = open(f"HoaDon#{i+1}.txt", "w")
+        flag = True
+        while i % 100000 != 0 or flag:
+            index = randint(0, 99999)
+            f.write("INSERT INTO HoaDon (MaHD, MaKH, NgayLap, TongTien) VALUES ")
+            f.write(f"('{bill_id[i]}', '{customer_id[index]}', '{get_date()}', '{total_bill[i]}')\n")
+            i += 1
+            flag = False
+        f.close()
+
+
+def readData(total_bill):
+    index = 0
+    for i in range(1, 11):
+        f = open(f"CT_HoaDon#{i}.txt", "r")
+        for line in f:
+            if line == "GO\n":
+                continue
+            info = line.split("INSERT [dbo].[CT_HoaDon] ([MaHD], [MaSP], [SoLuong], [GiaBan], [GiaGiam], [ThanhTien]) VALUES ")
+            data = info[-1].split(', ')
+            total_bill[index] += int(data[-1][:-2])
+            index += 1
+        if index == 500000:
+            index = 0
+        f.close()
+
+
+customer_id = []
+bill_id = []
+total_bill = []
+i = 1
+
+while i <= 500000:
+    zero = 6 - len(str(i))
+    if i <= 100000:
+        customer_id.append('KH' + '0' * (zero-1) + str(i-1))
+    bill_id.append('HD' + '0' * zero + str(i))
+    total_bill.append(0)
+    i += 1
+readData(total_bill)
+writeToScript(bill_id, customer_id, total_bill)
