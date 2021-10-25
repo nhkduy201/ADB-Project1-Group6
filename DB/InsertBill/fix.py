5ce1a8548bf8217e3ebf2347c33927c664aeecd0
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2021-10-25 21:55:40 +0700

    Fix HoaDon and CT_HoaDon

--- DB/InsertBill/fix.py
+++ DB/InsertBill/fix.py
@@ -14,7 +14,20 @@ def readData():
     return product_id, price
 
 
-def writeData(product_id, price):
+def readFromFile():
+    current_product = []
+    for i in range(1,6):
+        f = open(f"CT_HoaDon#{i}.txt", "r")
+        for line in f:
+            if line == "GO\n":
+                continue
+            info = line.split("INSERT [dbo].[CT_HoaDon] ([MaHD], [MaSP], [SoLuong], [GiaBan], [GiaGiam], [ThanhTien]) VALUES ")
+            data = info[-1].split(", ")
+            current_product.append(data[1][2:-1])
+    return current_product
+
+
+def writeData(product_id, price, current_product):
     i = 1
     for j in range(6, 11):
         f = open(f"CT_HoaDon#{j}.txt", "w")
@@ -22,15 +35,19 @@ def writeData(product_id, price):
             quantity = random.randint(1, 10)
             sale = random.randint(1, 5)
             index = random.randint(0, 9999)
+            while current_product[i-1] == product_id[index]:
+                index = random.randint(0, 9999)
             price_before = int(price[index] * sale * 0.1)
             price_after = int(price[index] * sale * 0.01)
             f.write("INSERT [dbo].[CT_HoaDon] ([MaHD], [MaSP], [SoLuong], [GiaBan], [GiaGiam], [ThanhTien]) VALUES ")
-            f.write(f"(N'HD{(6 - len(str(i))) * '0' + str(i)}', N'{product_id[index]}, {quantity}, {price_before}, {price_after}, {quantity*(price_before-price_after)})\n")
+            f.write(f"(N'HD{(6 - len(str(i))) * '0' + str(i)}', N'{product_id[index]}', {quantity}, {price_before}, {price_after}, {quantity*(price_before-price_after)})\n")
             if i % 100 == 0:
                 f.write("GO\n")
             i += 1
         f.close()
 
 
+current_product = readFromFile()
 product_id, price = readData()
-writeData(product_id, price)
+writeData(product_id, price, current_product)
+
