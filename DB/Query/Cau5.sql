54381e54d9f252472ca0baa95fa4549cc716e108
Nguyễn Thị Như Phương <92770267+nhuphuongs@users.noreply.github.com>
2021-11-14 00:31:04 +0700

    Update Cau5.sql

--- DB/Query/Cau5.sql
+++ DB/Query/Cau5.sql
@@ -3,8 +3,17 @@ go
 
 --a
 select *
-from HoaDon hd join CT_HoaDon ct on hd.MaHD=ct.MaHD join SanPham sp on ct.MaSP = sp.MaSP
+from HoaDon hd join CT_HoaDon ct on hd.MaHD = ct.MaHD join SanPham sp on ct.MaSP = sp.MaSP
 
 select * 
 from HoaDon hd, CT_HoaDon ct, SanPham sp
-where hd.MaHD=ct.MaHD and ct.MaSP = sp.MaSP
+where hd.MaHD = ct.MaHD and ct.MaSP = sp.MaSP
+
+--b
+select *
+from HoaDon HD join KhachHang KH
+on HD.MaKH = KH.MaKH
+
+select *
+from KhachHang KH join HoaDon HD
+on KH.MaKH = HD.MaKH
